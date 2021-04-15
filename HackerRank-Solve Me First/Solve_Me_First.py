58c5424ce05f950b8b8f50760c6ff051504afa94
rsandhusdg <80769623+rsandhusdg@users.noreply.github.com>
2021-04-15 16:04:03 -0400

    Update Solve_Me_First.py

--- HackerRank-Solve Me First/Solve_Me_First.py
+++ HackerRank-Solve Me First/Solve_Me_First.py
@@ -1,3 +1,41 @@
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+a=7
+b=3
+Return 10.
+
+Function Description:
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+    int a: the first value
+    int b: the second value
+
+Returns
+- int: the sum of a and b
+
+Constraints
+1<=a,b<=1000
+
+Sample Input
+
+a = 2
+b = 3
+
+Sample Output
+
+5
+
+Explanation
+
+2+3=5. 
+
+Solution in Python3
+
 def solveMeFirst(a,b):
     return a+b
 
@@ -5,3 +43,19 @@ num1 = int(input())
 num2 = int(input())
 value = solveMeFirst(num1,num2)
 print(value)
+
+Test Case 0:
+    
+Compiler Message
+
+Success
+
+Input (stdin)
+
+    2
+
+    3
+
+Expected Output
+
+    5
